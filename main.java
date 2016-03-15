ad37681d15c768b5b864789d6dd4db537241dc2c
lq <ineweyer@163.com>
2016-03-15 20:08:18 +0800

    add an new file & change main.java

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class Action {
        public static void main(String[] s) {
             System.out.printf("This is a test for java");
+	    System.out.printf("This change is on the another computer");
 	}
 }
